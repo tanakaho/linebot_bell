6c18b46bd71459b340527f71d8150efa8f870c57
kaho <57692753+tanakaho@users.noreply.github.com>
2022-01-10 18:42:36 +0900

    署名復活

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // 署名を検証
-// const crypto = require('crypto');
-//     //チャンネルシークレットを秘密鍵として、HMAC-SHA256を使用しリクエストボディのダイジェスト値を取得
-// const channelSecret = process.env.LINE_CHANNEL_SECRET;
-// const body = '...';
-//     //ダイジェスト値をBase64エンコードした値と、リクエストヘッダーのx-line-signatureに含まれる署名が一致することを確認
-// const signature = crypto
-//     .createHmac('SHA156', channelSecret)
-//     .update(body).digest('base64');
+const crypto = require('crypto');
+    //チャンネルシークレットを秘密鍵として、HMAC-SHA256を使用しリクエストボディのダイジェスト値を取得
+const channelSecret = process.env.LINE_CHANNEL_SECRET;
+const body = '...';
+    //ダイジェスト値をBase64エンコードした値と、リクエストヘッダーのx-line-signatureに含まれる署名が一致することを確認
+const signature = crypto
+    .createHmac('SHA156', channelSecret)
+    .update(body).digest('base64');
 
 // モジュールのインポート
 const server = require("express")();
